3ae79d43974ba2c970c4aaba1d13112afce26381
HamudiHasan <hhsonet@gmail.com>
2019-08-19 23:35:08 +0600

    drawer added

--- app/src/main/java/com/uiutesseract/bloodbank/ui/activity/MainActivity.java
+++ app/src/main/java/com/uiutesseract/bloodbank/ui/activity/MainActivity.java
@@ -1,18 +1,66 @@
 package com.uiutesseract.bloodbank.ui.activity;
 
+import android.annotation.SuppressLint;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Spinner;
 
 import com.uiutesseract.bloodbank.R;
 
 // b-Hero
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  NavigationView.OnNavigationItemSelectedListener {
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Spinner staticSpinner = (Spinner) findViewById(R.id.spinner);
+
+
+        // For toolbar
+        toolbar = findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // set Drawer for others options
+        setDrawer();
+    }
+    /**
+     * Set navigation Drawer
+     */
+    private void setDrawer() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @SuppressLint("MissingPermission")
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+
+      /*  if (id == R.id.nav_book_a_space) {
+            // AppUtil.Toast("Book A Parking Space", this);
+        } */
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
